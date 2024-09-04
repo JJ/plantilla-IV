5f7ba804be6b1b8314b319aa29f4dcc7cb5597f9
JJ Merelo <jjmerelo@gmail.com>
2024-09-04 13:05:06 +0200

    #6 salir del workflow si hay un error

--- src/check-version.pl
+++ src/check-version.pl
@@ -9,7 +9,7 @@ use GitHub::Actions;
 my $correctVersion = "v0." . $ENV{'OBJETIVO'};
 
 if ( index( $ENV{'THIS_VERSION'}, $correctVersion ) == -1 ) {
-  error( "La versión $ENV{'THIS_VERSION'} es incorrecta: debería comenzar con $correctVersion");
+  set_failed( "La versión $ENV{'THIS_VERSION'} es incorrecta: debería comenzar con $correctVersion");
 } else {
   debug( "La versión $ENV{'THIS_VERSION'} es adecuada para este objetivo" );
 }
