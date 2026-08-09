7429240d612dbee3a3003aeeaf4a426a72ed72f3
JJ Merelo <jjmerelo@gmail.com>
2026-08-09 12:01:29 +0200

    #71 comprobación de versión

--- src/check-version.pl
+++ src/check-version.pl
@@ -11,6 +11,9 @@ my $correctVersion = "v0." . $ENV{'OBJETIVO'};
 if ( index( $ENV{'THIS_VERSION'}, $correctVersion ) == -1 ) {
   set_failed( "La versión $ENV{'THIS_VERSION'} es incorrecta: debería comenzar con $correctVersion");
 } else {
+  if ( $ENV{'THIS_VERSION'} eq 'v0.0.0' ) {
+    set_failed( "Una versión 0.0.0 es incorrecta; la primera versión posible es la 0.0.1" );
+  }
   debug( "La versión $ENV{'THIS_VERSION'} es adecuada para este objetivo" );
 }
 
